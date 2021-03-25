694ff0368c7003525135cc67969aec0990c0f2e0
Myra Smith <61550209+myrasmith@users.noreply.github.com>
2021-03-25 12:11:16 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,14 @@
+// Suggestion: Use <a> tags with the href and target attributes for your links -
+// the default behavior works the same way your custom click listeners work!
+
 document.addEventListener('DOMContentLoaded', eve => {
 //  ====================
 // |  Page Navigation   |
 //  ====================
   document.querySelectorAll('.nav').forEach(spaner => {
     spaner.addEventListener('click', evn => {
+      // I think you can simplify this! Instead of using querySelectorAll,
+      // could you use querySelector to grab only the section you need?
       document.querySelectorAll('.section').forEach(section => {
         if (section.id.includes(spaner.getAttribute('href'))) {
           window.open(`#${spaner.getAttribute('href')}`, '_self')
@@ -16,14 +21,19 @@ document.addEventListener('DOMContentLoaded', eve => {
 // |  Show Less Button  |
 //  ====================
 
+  // Because you use #moreGit multiple times in the event listener,
+  // I'd recommend storing the DOM element itself instead of just its innerHTML
+  // That way, you don't have to use document.getElementById so many times.
   const moreMore = document.getElementById('moreGit').innerHTML
 
   let showMore = document.getElementById('show')
-  showMore.addEventListener('click', () => {
+  showMore.addEventListener('click', () => { 
     if (showMore.textContent === 'Show Less') {
       showMore.textContent = 'Show More'
       document.getElementById('moreGit').innerHTML = ''
+      // Hmmm... does the line below this do anything? .setAttribute() is a method, not a property!
       document.getElementById('moreGit').setAttribute = ('display', 'grid')
+      // Remove console logs before publishing!
       console.log(document.getElementById('moreGit').attributes)
     } else {
       showMore.textContent = 'Show Less'
@@ -35,6 +45,9 @@ document.addEventListener('DOMContentLoaded', eve => {
 // |   GitHub & Live    |
 //  ====================
 
+  // Don't Repeat Yourself! You could define a single click handler function that you use for
+  // each listener!
+  
   let linkSnowman = document.getElementById('linkSnowman')
   linkSnowman.addEventListener('click', () => {
     let location = linkSnowman.getAttribute('href')
@@ -77,3 +90,5 @@ document.querySelectorAll('.footer-elem').forEach(contact => {
   })
 })
 })
+
+// Potential additions: functionality for the .navigator button!
